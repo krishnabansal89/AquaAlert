f6988bdb19b6a6f77f96f406d1d3db84f15ea903
Krishna Bansal <krishnabansal890@gmail.com>
2024-05-28 23:56:18 +0530

    Super user removed

--- user/controller.ts
+++ user/controller.ts
@@ -1,5 +1,5 @@
 import User from "./userModel";
-import SuperUser from "./superuserModel";
+
 import jwt from "jsonwebtoken";
 import { Request , Response } from "express";
 import Room from "../room/roomModel";
@@ -49,53 +49,6 @@ const userController = {
             
     }
 }
-const superUserController = {
-    signup: async (req : Request, res : Response) => {
-        try {
-            const { userId , name, email, password, desc } = req.body;
-            const superUser = new SuperUser({
-                userId,
-                name,
-                email,
-                password,
-                desc,
-        
-            });
-            await superUser.save();
-            res.status(200).send(superUser);
-        } catch (error) {
-            res.status(400).send("Data is not Valid");
-        }
-    },
-    login: async (req : Request, res : Response) => {
-        try {
-            const { email, password } = req.body;
-            const superUser = await SuperUser.findOne({ email, password });
-            
-            if(superUser) {
-                res.status(200).send(superUser);
-                const jwtToken :jwt = jwt.sign({ email: superUser.email, _id: superUser._id }, jwtSecret );
-                res.status(200).json({ jwtToken });
-            }}
-        catch (error) {
-            res.status(400).send("Data is not Valid");
-        }
-    },
-    getUsers : async (req : Request, res : Response) => {
-        const token = req.body.token
-        const decoded = jwt.verify(token, jwtSecret);
-        const users:any = await SuperUser.find({ ownerId : decoded.id });
-        let userData:{houseNo :String , userName :string , waterSpend :number , userId:string };
-        let userDatas:any = [];
-        users.forEach(user => {
-            userData.houseNo = user.houseNo;
-            userData.userName = user.name;
-            userData.waterSpend = user.waterSpend;
-            userData.userId = user.userId;
-            userDatas.push(userData);
-        });
-        return res.status(200).json(userDatas);
-    }
-}
 
-export {userController , superUserController};
+
+export {userController};
